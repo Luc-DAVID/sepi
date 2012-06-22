f29ec664bf2cc624ed6ab39c9f1696a276c85b15
Luc DAVID <luckylazarus@free.fr>
2012-06-22 17:40:30 +0200

    First lazarus/fpc modification

--- programs/SepiDelphiUnitImporter/SepiDelphiUnitImporter.dpr
+++ programs/SepiDelphiUnitImporter/SepiDelphiUnitImporter.dpr
@@ -506,7 +506,7 @@ end;
   @param Context           Contexte de compilation
   @param SourceFileName    Nom du fichier source à traiter
 *}
-procedure (Context: TImporterContext;
+procedure ProcessImport(Context: TImporterContext;
   const SourceFileName: TFileName);
 var
   SourceFile: TStrings;
